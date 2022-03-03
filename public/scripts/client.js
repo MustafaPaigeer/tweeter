9ff76d401f5dc4cd9b0ee93ca1d8d91725e73742
Mustafa Paigeer <paigeer2012@gmail.com>
2022-03-03 08:47:01 +0000

    Refetch tweet on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,53 +5,57 @@
  */
 
 
-$(document).ready(function() {
-    $('form').on('submit', function(event) {
-        event.preventDefault();
-     
-        $('form').validate({
-            rules: {
-              text : {
-                required: true,
-                minlength: 1,
-                maxlength: 140
-              },
-            },
-            messages : {
-              text: {
-                minlength: "Tweets cannot be empty character",
-                maxlength: "Tweet shouldn't be more than 140 characters"
-              }}});
-    
-        // const tweetText = $('#tweet-text').val();
-        // if (tweetText.length < 1) {
-        //     alert("Form cannot be empty")
-        // } else if (tweetText.length > 140) {
-        //     alert ("form cannot exceed 150 char")
-        // } else {
-            let serializedData = $('form').serialize();
-        
-            $.ajax({
-                url: "/tweets",
-                type: "post",
-                data: serializedData
-            })
-            .then((data) => {
-    
- 
-            })
-        // }
+$(document).ready(function () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+
+    $('form').validate({
+      rules: {
+        text: {
+          required: true,
+          minlength: 1,
+          maxlength: 140
+        },
+      },
+      messages: {
+        text: {
+          minlength: "Tweets cannot be empty character",
+          maxlength: "Tweet shouldn't be more than 140 characters"
+        }
+      }
     });
 
+    // const tweetText = $('#tweet-text').val();
+    // if (tweetText.length < 1) {
+    //     alert("Form cannot be empty")
+    // } else if (tweetText.length > 140) {
+    //     alert ("form cannot exceed 150 char")
+    // } else {
+    let serializedData = $('form').serialize();
+   
+    $.ajax({
+      url: "/tweets",
+      type: "post",
+      data: serializedData
+    })
+      .then((data) => {
+        loadTweets();
+        $('#tweet-text').val('');
+      })
+    // }
+  });
+
   const loadTweets = () => {
-        $.ajax('/tweets', { method: 'GET' })
-        .then((tweets) => {
-          renderTweets(tweets);
-        });
-   };
+    $.ajax('/tweets', { method: 'GET' })
+      .then((tweets) => {
+        renderTweets(tweets);
+      });
+  };
+
   loadTweets()
-      const createTweetElement = (tweet) => {
-        const myTweet = `
+
+  const createTweetElement = (tweet) => {
+    const myTweet = `
         <article class="tweet">
         <header>
             <div>
@@ -71,17 +75,16 @@ $(document).ready(function() {
         </footer>
         </article>
         `
-        return myTweet;
-    }
-    
-    const renderTweets = function(tweets) {
-        // loops through tweets
-        // calls createTweetElement for each tweet
-        // takes return value and appends it to the tweets container
-          for (const tweetData in tweets) {
-              const $tweet = createTweetElement(tweets[tweetData]);
-              $('#tweetContainer').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-          }
-      }
+    return myTweet;
+  }
 
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweetData in tweets) {
+      const $tweet = createTweetElement(tweets[tweetData]);
+      $('#tweetContainer').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  }
 });
